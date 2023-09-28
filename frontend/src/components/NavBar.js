e17400e5b2932ea8621648552701797fb4d954c3
ScottA27 <scotta27501@gmail.com>
2023-09-28 11:21:46 +0000

    Attempting to fix the logged out navlinks

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -80,7 +80,6 @@ const NavBar = () => {
         className={styles.NavLink}
         activeClassName={styles.Active}
         to="/signin"
-        exact
       >
         <i className="fas fa-sign-in-alt"></i>Sign in
       </NavLink>
@@ -88,7 +87,6 @@ const NavBar = () => {
         className={styles.NavLink}
         activeClassName={styles.Active}
         to="/signup"
-        exact
       >
         <i className="fas fa-user-plus"></i>Sign up
       </NavLink>
